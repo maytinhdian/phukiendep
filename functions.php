3f73890ae27da3ce21fa02827e95c0de4f5a6379
Minh Triet <minhtriet@maytinhdian.com>
2025-04-24 07:43:03 +0700

    Media queries

--- functions.php
+++ functions.php
@@ -51,7 +51,7 @@ function phukiendep_setup()
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__('Primary', 'phukiendep'),
+			'nav-menu' => esc_html__('Primary', 'phukiendep'),
 		)
 	);
 
--- template-parts/header/header-bottom.php
+++ template-parts/header/header-bottom.php
@@ -1,17 +1,16 @@
-<div class="header-bottom">
+<div class="header-bottom responsive-test>
 
-    <nav id="site-navigation" class="header-bottom__main-navigation">
-        <!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'phukiendep'); ?></button> -->
-        <?php
-        wp_nav_menu(
-            array(
-                'theme_location' => 'menu-1',
-                'menu_id'        => 'primary-menu',
-                'menu_class'=> 'header-bottom__main-menu',
-                'before'=>'<h1>',
-                'after'=> '</h1>',
-            )
-        );
-        ?>
+    <nav id=" site-navigation class="header-bottom__main-navigation">
+    <!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'phukiendep'); ?></button> -->
+    <?php
+    wp_nav_menu(
+        array(
+            'theme_location' => 'nav-menu',
+            'menu_id'        => 'primary-menu',
+            'menu_class' => 'nav-menu',
+            'container_class' => 'header-bottom__main-menu',
+        )
+    );
+    ?>
     </nav><!-- #site-navigation -->
 </div>
