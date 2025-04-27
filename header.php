1496e2c6707b252ef717d6a037da86eb77684d0c
Minh Triet <minhtriet@maytinhdian.com>
2025-04-27 10:51:38 +0700

    Grid Responsive

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
-	<div id="page" class="site">
+	<div id="page" class="main-container">
 		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'phukiendep'); ?></a>
 
 		<header id="masthead" class="site-header">
