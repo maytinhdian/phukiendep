2b59a194e7e85446561f32599c4317851f06c5e1
Minh Triet <minhtriet@maytinhdian.com>
2025-04-30 06:46:53 +0700

    Fix Awesome - Dart Sass Depreacte functionss

--- template-parts/header/header-main.php
+++ template-parts/header/header-main.php
@@ -1,24 +1,10 @@
 <div class="header-main">
-
-    <div class="header-main__site-branding">
-        <?php
+    <?php 
+    // Check if the current page is the home page
+    if(is_front_page() || is_home()) {
+        //Display the logo
         the_custom_logo();
-        if (is_front_page() && is_home()) :
-        ?>
-            <h1 class="header-main__site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-        <?php
-        else :
-        ?>
-            <p class="header-main__site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-        <?php
-        endif;
-        $phukiendep_description = get_bloginfo('description', 'display');
-        if ($phukiendep_description || is_customize_preview()) :
-        ?>
-            <p class="header-main__site-description"><?php echo $phukiendep_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
-                                        ?></p>
-        <?php endif; ?>
-    </div><!-- .site-branding -->
-
-
+    }
+    
+    ?>
 </div>
